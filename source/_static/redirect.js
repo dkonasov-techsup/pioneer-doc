1c1b0ce063da6c398e0d005f78a942a8c2378f1b
dkonasov-techsup <d.konasov@geoscan.aero>
2024-04-01 16:04:56 +0300

    rdr without verification

--- source/_static/redirect.js
+++ source/_static/redirect.js
@@ -1,13 +1,6 @@
-document.addEventListener("DOMContentLoaded", () => {   
-    const url = window.location;
-    const loc = url.pathname;
-    const q = url.search.substr(1);
+"use strict"
+document.addEventListener("DOMContentLoaded", () => {
 
-    if(loc == "pioneer-doc.readthedocs.io/ru/master/index.html"){
-      window.location.href = "https://docs.geoscan.ru/pioneer/";
-    }
-    else{
-      console.log(loc + 'rdr error')
-    }
+  window.location.href = "https://docs.geoscan.ru/pioneer/";
 
-  });
+});
